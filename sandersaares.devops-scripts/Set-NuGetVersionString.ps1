ade11891a3e976c0aa1cc9891684adeac2ba8049
Sander Saares <sasaares@microsoft.com>
2021-04-23 14:25:28 +0300

    1.1.0 support branch-prefixed version string for nuget

--- sandersaares.devops-scripts/Set-NuGetVersionString.ps1
+++ sandersaares.devops-scripts/Set-NuGetVersionString.ps1
@@ -18,7 +18,12 @@ function Set-NuGetVersionString {
         # Cannot be set together with previewVersion.
         # If neither is set, marks the published version as a CB version.
         [Parameter()]
-        [switch]$stableVersion
+        [switch]$stableVersion,
+
+        # Allows the version number to be resolved when using a non-default branch (with the branch name in front of the version string).
+        # For NuGet packages we just trim the branch name since NuGet Gallery does not support the notion of branches - they are all mixed.
+        [Parameter()]
+        [switch]$allowFromBranch
     )
 
     # Output is the TFS process variable NuGetPackageVersion.
@@ -26,6 +31,17 @@ function Set-NuGetVersionString {
     if ($previewVersion -and $stableVersion) {
         Write-Error "Cannot set both previewVersion and stableVersion."
         return
+    }
+
+    # If we expect branch-specific version strings as input, we trim everything up to the first number, to get rid of any branch name prefix.
+    if ($allowFromBranch) {
+        $branchlessBuildNumber = $buildNumber -replace "^(.*?)(\d+\.\d+\.\d+-.*)$", '$2'
+
+        if ($branchlessBuildNumber -ne $buildNumber) {
+            Write-Host "Removed branch name from version string: $branchlessBuildNumber"
+        }
+
+        $buildNumber = $branchlessBuildNumber
     }
 
     # Expected input: 1.2.3-XXXXXX-YYYYYYY
